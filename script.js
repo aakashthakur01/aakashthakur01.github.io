8949e57909e8fb2da4f4d5cd61be3ae58ac9db39
Aakash <luciferaakash777@gmail.com>
2025-06-19 16:08:09 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Optional JavaScript can go here
+
+// Smooth scroll effect (optional enhancement)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
